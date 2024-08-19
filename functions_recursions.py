f891e4a0525d8a4109169c68596ed80dde22ac62
Avishek roy <royavishek711@gmail.com>
2024-08-20 02:42:13 +0530

    prac func rec

--- functions_recursions.py
+++ functions_recursions.py
@@ -39,5 +39,88 @@
 
 #----------------------------------------------------------------------------------------------
 
-# prime number using recursion
+# find greatest number
+# def greatest(n1,n2, n3):
+#     if n1>n2 and n1>n3:
+#         return n1
+#     elif n2>n1 and n2>n3:
+#         return n2
+#     else:
+#         return n3
 
+# n1,n2,n3=map(int,input('Enter three numbers: ').split())
+# print(greatest(n1,n2,n3))
+
+#----------------------------------------------------------------------------------------------
+
+# fahrenheit to celcius
+
+# def f_to_c(fahr):
+#     return ((fahr-32)*5/9)
+
+# def c_to_f(cel):
+#     return (cel*(9/5)+32)
+
+# user=input('Enter the temp unit in f or c: ')
+# if user=='f':
+#     print(f_to_c(float(input('Enter the temp in f: '))))
+# elif user=='c':
+#     print(c_to_f(float(input('Enter the temp in c: '))))
+# else:
+#     print('Invalid option')
+
+# -----------------------------------------------------------------------------------------------
+
+# sum of n natural numbers
+
+# def sum_of_n(n):
+#     if n==0:
+#         return 0
+#     return n+sum_of_n(n-1)
+
+# n=int(input('Enter the n natural number: '))
+# print(f'sum of first {n} natural numbers are: {sum_of_n(n)}')
+
+# -----------------------------------------------------------------------------------------------
+
+# pattern using recursion
+# ***
+# **
+# *
+
+# def pattern(n):
+#     if n==1:
+#         return '*'
+#     print('*'*n)
+#     return pattern(n-1)
+
+# n=int(input('Enter the num of rows: '))
+# print(pattern(n))
+
+# -----------------------------------------------------------------------------------------------
+
+#remove a word from a list
+
+# def rm_word(lst,word):
+#     newlst = []
+#     for i in lst:
+#         if i!=word:
+#             newlst.append(i.strip(word))
+#     return newlst
+
+# lst=['hello','world','my','name','is','Avishek','world','Avishek','rimon','hello']
+# word=input("Enter the word to remove: ")
+# print(rm_word(lst,word))
+
+# -----------------------------------------------------------------------------------------------
+
+#multiplication table using recursion
+
+# def table(num,n):
+#     if n<=10:
+#         print(f'{num} * {n} = {num*n}')
+#         return table(num,n+1)
+#     else:
+#         return
+# num=int(input('enter the num for table: '))
+# table(num,1)
