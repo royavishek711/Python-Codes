87bd390fc7946bb84f424ece44a6375dfc4e8e56
Avishek roy <royavishek711@gmail.com>
2024-09-08 16:00:09 +0530

    insertion_recursive

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,35 @@
-def insertionSort(arr, n):
-        #code here
+# def insertionSort(arr, n):
+#         #code here
         
-        for i in range(n-1):
+#         for i in range(n-1):
             
-            for j in range(i+1, 0, -1):
+#             for j in range(i+1, 0, -1):
                 
-                if arr[j]<arr[j-1]:
-                    arr[j], arr[j-1]=arr[j-1], arr[j]
-                else:
-                    break
+#                 if arr[j]<arr[j-1]:
+#                     arr[j], arr[j-1]=arr[j-1], arr[j]
+#                 else:
+#                     break
 
         
+#         return arr
+
+# recursive
+def insertionSort(arr, i, n):
+        #code here
+        
+        if i==n:
+            return
+            
+        for j in range(i, 0, -1):
+            
+            if arr[j]<arr[j-1]:
+                arr[j], arr[j-1]=arr[j-1], arr[j]
+            else:
+                break
+        
+        insertionSort(arr, i+1, n)
         return arr
 
 arr=[4,6,2,5,5,2,4]
 
-print(insertionSort(arr, len(arr)))
+print(insertionSort(arr, 0, len(arr)))
