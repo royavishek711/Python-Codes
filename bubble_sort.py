ec7cfccd45d603fc0bd3ab23e99f291e5471f86e
Avishek roy <royavishek711@gmail.com>
2024-09-08 15:37:57 +0530

    recursive bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,18 +1,37 @@
-def bubbleSort(arr, n):
-    # code here
+# def bubbleSort(arr, n):
+#     # code here
     
-    for i in range(n):
-        swapped=False
+#     for i in range(n):
+#         swapped=False
         
-        for j in range(n-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j], arr[j+1]=arr[j+1], arr[j]
-                swapped=True
+#         for j in range(n-i-1):
+#             if arr[j]>arr[j+1]:
+#                 arr[j], arr[j+1]=arr[j+1], arr[j]
+#                 swapped=True
         
-        if not swapped:
-            break
+#         if not swapped:
+#             break
+#     return arr
+
+#recursive
+        
+def bubbleSort(arr, n):
+# code here
+    if n==1:
+        return 
+    
+    swapped=False
+    
+    for j in range(n-1):
+        if arr[j]>arr[j+1]:
+            arr[j], arr[j+1]=arr[j+1], arr[j]
+            swapped=True
+    
+    if not swapped:
+        return
+    
+    bubbleSort(arr, n-1)
     return arr
 
-arr=[2,4,6,1,2,7,8]
-
-print(bubbleSort(arr, len(arr)))
+arr=[2,4,0,6,1,2,7,8]
+print(bubbleSort(arr, len(arr)-1))
